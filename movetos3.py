460bcf46b70d962d1fa14859278b365dce6381be
V David Zvenyach <vdavez@gmail.com>
2014-07-25 13:53:09 -0400

    more updates, including s3

---
+++ movetos3.py
@@ -0,0 +1,14 @@
+import boto
+from boto.s3.connection import S3Connection
+import os
+
+pwd = os.getcwd()
+
+conn = S3Connection(os.environ["AWS_ACCESS_KEY_ID"], os.environ["AWS_SECRET_ACCESS_KEY"])
+b = conn.get_bucket(os.environ["BUCKET_NAME"])
+from boto.s3.key import Key
+
+for fn in os.listdir(pwd + "/files/"):
+	if (b.get_key(fn) == None):
+		k = b.new_key(fn)
+		k.set_contents_from_filename('files/' + fn)
