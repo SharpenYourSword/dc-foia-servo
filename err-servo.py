a9f46fbc9c2d48279cb49559d37719f065fd17f1
V David Zvenyach <vdavez@gmail.com>
2014-07-25 15:43:50 -0400

    working twitter functionality

--- err-servo.py
+++ err-servo.py
@@ -9,7 +9,7 @@ import requests
 from requests.exceptions import Timeout
 import json
 import subprocess
-import movetos3
+from twitterservo import tweetIt
 #import tweetaboutit
 
 dl = os.getcwd() + "/files/"
@@ -17,21 +17,20 @@ saved = json.loads(open("foia.json").read())
 exists = []
 for responses in saved:
 	exists.append(responses.get("fname"))
-print exists
 
 def checkNewFile(fname):
 	for r in exists:
-		if r == fname:
-			print "File Already Exists"
-			return False
+		if r == fname:		# Check to see if File exists
+			return False	# File already exists!
 	return True
 
-def download_file(url, cookiedict, fname, ftype):
+def download_file(url, cookiedict, fname, ftype, description):
+
 	local_filename = "files/" + fname + "." + ftype
 	if (os.path.exists(local_filename)):
 		return local_filename
-	elif checkNewFile(fname):
-		print "Hey, this is a new file"
+	elif checkNewFile(fname): # Check to see if File exists
+		print "Hey, this is a new file: " + local_filename 
 		viewstate = cookiedict["ASP.NET_SessionId"] + "_3"
 		new_url = "https://foia-dc.gov/ERR/palEleViewDocs.aspx?__EVENTTARGET=&__EVENTARGUMENT=&__LASTFOCUS=&CUSTOMVIEWSTATE_KEY=VIEWSTATE_" + viewstate + "&__VIEWSTATE=&pageno=1&relpath=&hiddocid=&hidisPalDoc=F&hidDocTypes=3%2C4%2C5%2C66%2C6%2C7%2C8%2C9%2C10%2C11%2C12%2C13%2C14%2C15%2C16%2C17%2C18%2C19%2C20%2C21%2C22%2C23%2C64%2C24%2C25%2C26%2C27%2C28%2C29%2C30%2C31%2C32%2C33%2C34%2C35%2C36%2C37%2C38%2C39%2C40%2C41%2C68%2C69%2C74%2C70%2C72%2C73%2C71%2C42%2C43%2C44%2C45%2C46%2C47%2C48%2C49%2C50%2C51%2C52%2C53%2C54%2C55%2C56%2C57%2C58%2C59%2C60%2C61%2C62%2C63%2C78%2C79&txtDocName=*&txtContent=&hidflg=Y&txtFrom=&txtTo=&pgNo=1&hidsrt=0&hidsrtfield=2&hidNoMoreVisibility=True&hiderrid=" + fname
 		try:
@@ -40,7 +39,8 @@ def download_file(url, cookiedict, fname, ftype):
 				for chunk in r.iter_content(chunk_size=4096):
 					fd.write(chunk)
 			#When you're here, it's time to tweet about it!
-			#And to upload to S3
+			tweetIt(description, fname, ftype)
+
 		except Timeout:
 			print "huh?"
 	return local_filename
@@ -92,8 +92,7 @@ while (still_more):
 
 	out += objs
 	for obj in objs:
-		print "printing " + obj['fname']
-		f = download_file("https://foia-dc.gov/Request/palContentType.aspx?type=3&DocID=" + obj["fname"] + "&isPALDoc=F", cookies, obj["fname"], obj["ftype"])
+		f = download_file("https://foia-dc.gov/Request/palContentType.aspx?type=3&DocID=" + obj["fname"] + "&isPALDoc=F", cookies, obj["fname"], obj["ftype"], obj["name"])
 	try:
 		next_pg = driver.find_element_by_xpath('//*[@id="image15"]')
 		next_pg.click()
@@ -104,4 +103,5 @@ while (still_more):
 with open("foia.json", 'wb') as fd:
   fd.write(json.dumps(out, indent=4))
 
+import movetos3
 print "All Done"
