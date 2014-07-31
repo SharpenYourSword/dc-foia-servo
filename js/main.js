ecdd0a312db4c117a4fc6f960044c1d5209054b6
V David Zvenyach <vdavez@gmail.com>
2014-07-30 21:28:52 -0400

    handle sort for JSON

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $.getJSON("https://s3.amazonaws.com/dcfoiaservo/foia.json", function (d) {
   var out = "<h2>FOIA Responses</h2><dl>";
-    for (var i in d) {
+    for (var i in sortObject(d,"pub",false)) {
       var description = d[i].name;
       var file = d[i].fname + "." + d[i].ftype
       var url = "https://s3.amazonaws.com/dcfoiaservo/" + file
@@ -10,3 +10,12 @@ $.getJSON("https://s3.amazonaws.com/dcfoiaservo/foia.json", function (d) {
     document.getElementById("foialist").innerHTML=out;
 
 })
+
+function sortObject(obj, prop, asc) {
+    obj = obj.sort(function(a, b) {
+        if (asc) return (a[prop] > b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0);
+        else return (b[prop] > a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0);
+    });
+    console.log()
+    return obj
+}
