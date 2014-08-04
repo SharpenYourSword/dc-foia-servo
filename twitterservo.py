62dfd5893987fe7d79c727912cc0efd11c2d8bfd
V David Zvenyach <vdavez@gmail.com>
2014-08-04 06:45:32 -0400

    ready for twitter

--- twitterservo.py
+++ twitterservo.py
@@ -6,7 +6,7 @@ def tweetIt (description, fname, ftype):
 	api = twitter.Api(consumer_key=os.environ["TW_CONSUMER_KEY"], consumer_secret=os.environ["TW_CONSUMER_SECRET"], access_token_key=os.environ["TW_TOKEN"],access_token_secret=os.environ["TW_TOKEN_SECRET"])
 	if len(description) > 99:
 		description = description[:99]
-	out_text = "New posting: " + description[:99] + " https://s3.amazonaws.com/dcfoiaservo/" + fname + "." + ftype
+#	out_text = "New posting: " + description[:99] + " https://s3.amazonaws.com/dcfoiaservo/" + fname + "." + ftype
 #	When we've converted existing PDFs to JSON, then turn this on!
-#	out_text = "New posting: " + description[:99] + " http://code.esq.io/dc-foia-servo/response#" + fname
+	out_text = "New posting: " + description[:99] + " http://code.esq.io/dc-foia-servo/response#" + fname
 	status = api.PostUpdate(out_text)
